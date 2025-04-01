84818053effe97f9d94cda1a8bf8fee3c58d83b7
민예린 <167814578+Yerineeeee98@users.noreply.github.com>
2025-04-01 11:40:44 +0900

    좋아요 기능 관련 모델링

--- posts/models.py
+++ posts/models.py
@@ -12,7 +12,15 @@ class Post(models.Model):
         crop=['middle', 'center'],
         upload_to='image/%Y/%m'
     )
+    # 작성자
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    
+    # 게시물을 좋아요 누른 사람들, m : n으로 연결하고싶음
+    like_users = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name = 'like_posts', # 작성자와 게시물을 좋아요 누른 사람들 사이에 역참조가 충돌해서 새로 설정하기 
+        ) 
+    
     
 class Comment(models.Model):
     content = models.CharField(max_length=200)
