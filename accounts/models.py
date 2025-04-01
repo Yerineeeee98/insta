b18d3cfefad1c9f599120a89953b1abeac20dfcd
민예린 <167814578+Yerineeeee98@users.noreply.github.com>
2025-04-01 14:05:05 +0900

    followings modeling/migration

--- accounts/models.py
+++ accounts/models.py
@@ -10,3 +10,5 @@ class User(AbstractUser):
         crop=['middle', 'center'],
         upload_to='image/%Y/%m'
     )
+    followings = models.ManyToManyField('self', related_name = 'followers', symmetrical=False) # 내가 팔로우한 사람 m:n # self는 나 스스로를 m:n으로 연결
+    # symmetrical 대칭적인가 false를 줌으로써 단방향을 의미 
